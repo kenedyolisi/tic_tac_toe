fc6547177c6bd8be8509b1a4cca37f3f4222d8be
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2025-01-03 12:34:03 +0300

    feat: create square component for tic-tac-toe game

---
+++ src/components/square.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  interface Props {
+    value: string;
+    handleClick: () => void;
+  }
+  let { value, handleClick }: Props = $props();
+</script>
+
+<button class="border hover:bg-gray-100" type="button" onclick={handleClick}>
+  {value}
+</button>
